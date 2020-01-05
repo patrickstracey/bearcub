c4dd565022a31a8df0da38b1332bc91730414794
Patrick <patrick@pop-os.localdomain>
2020-01-05 15:07:18 -0800

    secondary styling stuff

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -4,10 +4,14 @@ import { DashboardComponent } from './dashboard/dashboard/dashboard.component';
 import { BusinessComponent } from './settings/business/business.component';
 import { ClassesComponent } from './settings/classes/classes.component';
 import { OfferingsComponent } from './settings/offerings/offerings.component';
+import { OverviewComponent } from './customers/overview/overview.component';
+import { GeneratorComponent } from './payment-plan/generator/generator.component';
 
 
 const routes: Routes = [
   { path: '', component: DashboardComponent },
+  { path: 'family', component: OverviewComponent },
+  { path: 'plans', component: GeneratorComponent },
   { path: 'settings/business-info', component: BusinessComponent },
   { path: 'settings/classes', component: ClassesComponent },
   { path: 'settings/offerings', component: OfferingsComponent },
