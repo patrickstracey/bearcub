fe51494f9d929b2fbb5c848d5f5c7f2ec9a65f9e
Patrick <patrick@pop-os.localdomain>
2020-01-05 23:12:15 -0800

    offering mockup

--- frontend/src/app/material.module.ts
+++ frontend/src/app/material.module.ts
@@ -6,13 +6,18 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+
 
 
 @NgModule({
     imports: [ MatSidenavModule, MatToolbarModule, MatButtonModule,
-               MatIconModule, MatListModule, MatCardModule ],
+               MatIconModule, MatListModule, MatCardModule,
+               MatFormFieldModule, MatInputModule ],
     exports: [ MatSidenavModule, MatToolbarModule, MatButtonModule,
-               MatIconModule, MatListModule, MatCardModule ]
+               MatIconModule, MatListModule, MatCardModule,
+               MatFormFieldModule, MatInputModule ]
 })
 
 export class MaterialModule {}
--- frontend/src/app/settings/offerings/offerings.component.ts
+++ frontend/src/app/settings/offerings/offerings.component.ts
@@ -1,8 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
-
 //Essentially for accounting needs. Setup Pre-save Payment Plans, Rates, and Services here
-
 @Component({
   selector: 'app-offerings',
   templateUrl: './offerings.component.html',
@@ -10,9 +8,59 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OfferingsComponent implements OnInit {
 
+  private offerings = [
+    {
+      name:'5 Full Days [36 months - Pre-K]',
+      tuition:'163.00',
+      minimum: '145.00'
+    },
+    {
+      name:'5 Half Days [36 months - Pre-K]',
+      tuition:'133.00',
+      minimum: '105.00'
+    },
+    {
+      name:'4 Full Days [36 months - Pre-K]',
+      tuition:'153.00',
+      minimum: '135.00'
+    },
+    {
+      name:'4 Half Days [36 months - Pre-K]',
+      tuition:'123.00',
+      minimum: '115.00'
+    },
+    {
+      name:'3 Full Days [36 months - Pre-K]',
+      tuition:'141.00',
+      minimum: '120.00'
+    },
+    {
+      name:'3 Half Days [36 months - Pre-K]',
+      tuition:'113.00',
+      minimum: '98.00'
+    },
+    {
+      name:'2 Full Days [36 months - Pre-K]',
+      tuition:'108.00',
+      minimum: '85.00'
+    },
+    {
+      name:'2 Half Days [36 months - Pre-K]',
+      tuition:'88.00',
+      minimum: '78.00'
+    },
+    {
+      name:'Drop-in [36 months - Pre-K]',
+      tuition:'75.00',
+      minimum: '75.00'
+    }
+  ];
+
   constructor() { }
 
   ngOnInit() {
+
   }
+
 
 }
