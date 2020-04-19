34c85c74fbc214b0a832fbb5a6a2598908816f68
patrickstracey <pstracey17@gmail.com>
2020-04-19 13:49:11 -0700

    implemented check-in overview

--- frontend/src/app/_services_mock_data/name-generator.service.ts
+++ frontend/src/app/_services_mock_data/name-generator.service.ts
@@ -28,11 +28,22 @@ export interface RoomData {
   building_name: string;
 }
 
+export interface CheckinData {
+  student_name: string;
+  checkin_submitter: string;
+  checkin_type: string;
+  service_type: string;
+  scheduled_on: Date;
+  status: boolean;
+  payment: string;
+}
+
 const CLASSROOM_NAMES: string[] = ["Sunshine", "Burrow", "Moonlight", "Redwood", "Kelp", "Spaceship"];
 
 const NAMES: string[] = [
     'Maia', 'Asher', 'Olivia', 'Atticus', 'Amelia', 'Jack', 'Charlotte', 'Theodore', 'Isla', 'Oliver',
-    'Isabella', 'Jasper', 'Cora', 'Levi', 'Violet', 'Arthur', 'Mia', 'Thomas', 'Elizabeth'
+    'Isabella', 'Jasper', 'Cora', 'Levi', 'Violet', 'Arthur', 'Mia', 'Thomas', 'Elizabeth', 'Mary', 'Elton',
+    'Derrick', 'Quincey', 'Sharon', 'Kevin', 'Kim', 'Lee', 'Valerie', 'Kinsey', 'MacKenzie', 'Zeta'
   ];
 
 @Injectable({providedIn: 'root'})
@@ -48,7 +59,11 @@ export class MockNameGeneratorService {
         }
     }
 
-    createNewFamily() {
+    generateInt(floor: number, max: number): number{
+      return floor + (Math.floor(Math.random() * Math.floor(max)));
+    }
+
+    createNewFamily(): FamilyData {
       const name : NameInterface = this.generateName();
       const student_count = 1+ Math.round(Math.random()* 1.5);
       const status = true;
@@ -63,9 +78,9 @@ export class MockNameGeneratorService {
       }
     }
 
-    createNewStaffMember() {
+    createNewStaffMember(): StaffData {
       const name : NameInterface = this.generateName();
-      const wage = 8 + Math.floor(Math.random() * Math.floor(20))
+      const wage = this.generateInt(8, 20)
       const hired = new Date('2/26/2018');
     
       return {
@@ -86,13 +101,51 @@ export class MockNameGeneratorService {
         const new_room : RoomData = {
           classroom_name: classroom_name,
           building_name: "Building A",
-          square_footage: Math.floor(Math.random() * (4000 - 750) + 750),
+          square_footage: this.generateInt(750, 2100),
         }
         classrooms.push(new_room);
         })
         
         return classrooms;
       };
+    
+    createCheckin(): CheckinData{
+      const s_name = this.generateName();
+      const p_name = this.generateName();
+      let c_type = "Drop-In"
+      let s_type = "Full Day"
+      const today = new Date();
+      let scheduled = null;
+      let status: boolean = true;
+      let pment = 'Paid at Check-in'
 
+      if (this.generateInt(0, 2)  == 1){
+        status = false
+      }
+      if (this.generateInt(0, 4)  == 3){
+        s_type = "Half Day"
+      }
+      if (this.generateInt(0, 2)  == 1){
+        scheduled = new Date()
+        scheduled.setDate(today.getDate() - this.generateInt(1,6));
+        scheduled.toDateString();
+        c_type = "Scheduled Online"
+        pment = "Pre-paid Online"
+
+      }
+
+      const checkin =  {
+        student_name: s_name.first + " " + s_name.last,
+        checkin_submitter: p_name.first + " " + s_name.last,
+        checkin_type: c_type,
+        service_type: s_type,
+        scheduled_on: scheduled,
+        status: status,
+        payment: pment
+      }
+
+      return checkin;
+
+    }
 
 }
--- frontend/src/app/checkins/checkin-overview/checkin-overview.component.ts
+++ frontend/src/app/checkins/checkin-overview/checkin-overview.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MockNameGeneratorService, CheckinData } from 'src/app/_services_mock_data/name-generator.service';
 
 @Component({
   selector: 'app-checkin-overview',
@@ -7,9 +11,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CheckinOverviewComponent implements OnInit {
 
-  constructor() { }
+  displayedColumns: string[] = ['status', 'student_name', 'checkin_submitter', 'service_type', 'checkin_type', 'scheduled_on', 'payment'];
+  dataSource: MatTableDataSource<CheckinData>;
+
+  @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
+  @ViewChild(MatSort, {static: true}) sort: MatSort;
+
+  constructor(
+    private _mockDataService: MockNameGeneratorService
+  ) 
+  { 
+    let checkins: CheckinData[] = Array.from({length: 14}, (_, k) => this._mockDataService.createCheckin());
+    this.dataSource = new MatTableDataSource(checkins);
+  }
 
   ngOnInit() {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  }
+
+  applyFilter(filterValue: string) {
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
   }
 
 }
