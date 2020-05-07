80987317ea6111dc77edc95e004f845700aff4fc
patrickstracey <pstracey17@gmail.com>
2020-05-07 09:35:31 -0700

    refine mock photo service

--- frontend/src/app/_services_mock_data/photo-mocks.service.ts
+++ frontend/src/app/_services_mock_data/photo-mocks.service.ts
@@ -3,21 +3,30 @@ import { Injectable } from '@angular/core';
 
 const studentPath: string = '../../../assets/mock-photos/students/';
 const teacherPath: string = '../../../assets/mock-photos/teachers/';
-const studentFiles: string[] = ['student_1.webp'];
-const teacherFiles: string[] = ['student_1.webp'];
+const studentFiles: string[] = ['student_1.webp', 'student_2.jpg', 'student_3.jpg', 'student_4.jpg', 'student_5.jpg', 'student_6.jpg',];
+const teacherFiles: string[] = ['teacher_1.jpg'];
 
 @Injectable({providedIn: 'root'})
 export class PhotoMocksService {
 
   constructor() { }
 
-  getPhotos(group:string){
-    let files: string[] = []; 
+  getPhotos(group: 'student' | 'teacher' | 'all'){
     switch(group){
       case 'student':
         return this.compilePhotos(studentPath, studentFiles);
       case 'teacher':
         return this.compilePhotos(teacherPath, teacherFiles); 
+      case 'all':
+        let comboList: string[] = [];
+        this.compilePhotos(studentPath, studentFiles).forEach(photo=>{
+          comboList.push(photo);
+        });
+        this.compilePhotos(teacherPath, teacherFiles).forEach(photo=>{
+          comboList.splice(2, 0, photo);
+        });
+        return comboList;
+
     }
   }
 
