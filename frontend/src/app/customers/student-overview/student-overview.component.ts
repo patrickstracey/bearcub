09039611a59e93a1120d035e5d7d9b4475505cd8
patrickstracey <pstracey17@gmail.com>
2020-05-07 22:11:51 -0700

    add student table to class details

--- frontend/src/app/customers/student-overview/student-overview.component.ts
+++ frontend/src/app/customers/student-overview/student-overview.component.ts
@@ -4,18 +4,8 @@ import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
 import { BaseModalService } from 'src/app/_services/base-modal.service';
 import { NewStudentForms } from './student-overview.forms';
+import { MockNameGeneratorService, StudentData } from 'src/app/_services_mock_data/name-generator.service';
 
-export interface StudentData {
-  last_name: string;
-  first_name: string;
-  classroom: string;
-  dob: Date;
-}
-
-const NAMES: string[] = [
-  'Maia', 'Asher', 'Olivia', 'Atticus', 'Amelia', 'Jack', 'Charlotte', 'Theodore', 'Isla', 'Oliver',
-  'Isabella', 'Jasper', 'Cora', 'Levi', 'Violet', 'Arthur', 'Mia', 'Thomas', 'Elizabeth'
-];
 //Place to search and view all current customers
 
 @Component({
@@ -32,9 +22,10 @@ export class StudentOverviewComponent implements OnInit {
   @ViewChild(MatSort, {static: true}) sort: MatSort;
 
   constructor(
-    private _baseModalService : BaseModalService
+    private _baseModalService : BaseModalService,
+    private _mockGeneratorService: MockNameGeneratorService
   ) {
-    const users = Array.from({length: 58}, (_, k) => createNewUser(k + 1));
+    const users = Array.from({length: 58}, (_, k) => this._mockGeneratorService.createNewStudent(k + 1));
     // Assign the data to the data source for the table to render
     this.dataSource = new MatTableDataSource(users);
    }
@@ -57,16 +48,3 @@ export class StudentOverviewComponent implements OnInit {
   }
 
 }
-
-function createNewUser(id: number): StudentData {
-  const first_name = NAMES[Math.round(Math.random() * (NAMES.length - 1))];
-  const last_name = NAMES[Math.round(Math.random() * (NAMES.length - 1))];
-  const dob = new Date('10/15/2016');
-
-  return {
-    last_name: first_name,
-    first_name: last_name,
-    classroom: "Sunshine",
-    dob: dob
-    }
-  };
--- frontend/src/app/school/class-detail/class-detail.component.ts
+++ frontend/src/app/school/class-detail/class-detail.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
+
 import { PhotoMocksService } from 'src/app/_services_mock_data/photo-mocks.service';
+import { MockNameGeneratorService, StudentData } from 'src/app/_services_mock_data/name-generator.service';
 
 @Component({
   selector: 'app-class-detail',
@@ -8,11 +11,18 @@ import { PhotoMocksService } from 'src/app/_services_mock_data/photo-mocks.servi
 })
 export class ClassDetailComponent implements OnInit {
 
+  displayedStudentColumns: string[] = ['name', 'notes', 'birthday', 'status'];
+  dataSource: MatTableDataSource<StudentData>;
   photos: string[];
 
   constructor(
-    private _mockPhotoService: PhotoMocksService
-  ) { }
+    private _mockPhotoService: PhotoMocksService,
+    private _mockDataService: MockNameGeneratorService
+  ) {
+    const users = Array.from({length: 14}, (_, k) => this._mockDataService.createNewStudent(k + 1));
+    console.log(users);
+    this.dataSource = new MatTableDataSource(users);
+   }
 
   ngOnInit() {
     this.photos = this._mockPhotoService.getPhotos('all');
